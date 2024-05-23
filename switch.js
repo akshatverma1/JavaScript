649d5ccb4950c0f657c7f03d00545ab6b78154d8
akshatverma1 <akshaverma14@gmail.com>
2024-05-23 23:06:43 +0530

    23/05/2024

---
+++ switch.js
@@ -0,0 +1,12 @@
+let str="violet";
+switch (str) {
+    case "orange":console.log("orange");
+        break;
+    case "violet":console.log("violet");
+        break;
+    case "red":console.log("red");
+        break;
+
+    default:console.log("no color");
+        break;
+}
