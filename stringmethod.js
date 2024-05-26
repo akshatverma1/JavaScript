e9c35471afecc697da3ead9851e1f06b3b51bccb
akshatverma1 <akshaverma14@gmail.com>
2024-05-26 15:32:38 +0530

    26/05/2024

--- stringmethod.js
+++ stringmethod.js
@@ -1,2 +1,8 @@
 let str="                   akshat                      ";
 console.log(str.trim());
+let str1="Akshat Verma";
+console.log(str1.toUpperCase());
+let str2="string";
+console.log(str2.indexOf("u"));
+let str3="       study     ";
+console.log(str3.trim().toUpperCase());
