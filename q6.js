0b2a83bc77853b670f5ea67164c1b44a923a0333
akshatverma1 <akshaverma14@gmail.com>
2024-05-27 13:54:53 +0530

    27/05/2024

---
+++ q6.js
@@ -0,0 +1,8 @@
+// let char ='I';
+// // console.log(char.charCodeAt());
+// if(97<=char.charCodeAt()<=122){
+//     console.log("Small ALphabeta");
+// }else{
+//     console.log("Big Alphabeta");
+// }
+let str="akshat";
