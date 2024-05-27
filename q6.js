d50f04ebeb94e11568f3891a516bb62f17376209
akshatverma1 <akshaverma14@gmail.com>
2024-05-27 15:27:07 +0530

    loop

--- q6.js
+++ q6.js
@@ -1,14 +1,14 @@
-// let char ='I';
-// // console.log(char.charCodeAt());
-// if(97<=char.charCodeAt()<=122){
-//     console.log("Small ALphabeta");
-// }else{
-//     console.log("Big Alphabeta");
-// }
-let str="aksHat";
-let indx =3;
-if(str[indx]==str[indx].toLowerCase()){
-    console.log("LowerCASE");
+let char ='I';
+// console.log(char.charCodeAt());
+if(97<=char.charCodeAt()<=122){
+    console.log("Small ALphabeta");
 }else{
-    console.log("UpperCASE");
+    console.log("Big Alphabeta");
 }
+// let str="aksHat";
+// let indx =3;
+// if(str[indx]==str[indx].toLowerCase()){
+//     console.log("LowerCASE");
+// }else{
+//     console.log("UpperCASE");
+// }
