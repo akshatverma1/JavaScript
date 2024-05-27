bc548f1287064d7c2d036a7d37461b5860e630e6
akshatverma1 <akshaverma14@gmail.com>
2024-05-27 23:31:16 +0530

    27/05/2024

---
+++ Part-4/q2.js
@@ -0,0 +1,9 @@
+// Write a JS program to find the no of digits in a number
+let number = 287152;
+let count=0;
+let copy=number;
+while(copy>0){
+    count++;
+    copy= Math.floor(copy/10);
+}
+console.log(count);
