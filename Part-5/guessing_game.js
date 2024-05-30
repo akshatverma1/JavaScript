af4026cee331c082e537d24473f35feea9427739
akshatverma1 <akshaverma14@gmail.com>
2024-05-30 23:56:54 +0530

    30/05/2024

--- Part-5/guessing_game.js
+++ Part-5/guessing_game.js
@@ -3,10 +3,14 @@ let max = Math.random();
 let range = Math.floor(max*username);
 console.log(range);
 let guess = prompt("Enter the Suggestion Number");
+while(true){
 if(range==guess){
     console.log("Games Passed");
+    break;
 } else if(guess=="quite"){
     console.log("Game End");
+    break;
 }else{
-    guess = prompt("Enter the Suggestion Number");
+    guess = prompt("Enter the Suggestion Number again");
+}
 }
