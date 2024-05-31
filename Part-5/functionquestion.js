3d62cd96f1513c0f6c6b3f9fd1a69726e893cf9e
akshatverma1 <akshaverma14@gmail.com>
2024-05-31 16:50:52 +0530

    31/05/2024

--- Part-5/functionquestion.js
+++ Part-5/functionquestion.js
@@ -1,5 +1,16 @@
-function str(a,b,c,d){
-    let strrr=[a+b+c+d];
-    return strrr;
+// function str(a,b,c,d){
+//     let strrr=[a+b+c+d];
+//     return strrr;
+// }
+// console.log(str("akshat","verma","onkaam","ceo"));
+
+
+function conate(str){
+    let addstring="";
+    for(let i=0;i<str.length;i++){
+        addstring = addstring +str[i];
+    }
+    return addstring;
 }
-console.log(str("akshat","verma","onkaam","ceo"));
+let str =["akshat","s","sd","ff"];
+console.log(conate(str));
---
+++ Part-5/methods.js
@@ -0,0 +1,12 @@
+const obje = {
+    add: function(a,b){
+        return a+b;
+    },
+    mul: function(a,b){
+        return a*b;
+    },
+    sub: function(a,b){
+        return a-b;
+    }
+}
+console.log(obje.mul(1,2));
