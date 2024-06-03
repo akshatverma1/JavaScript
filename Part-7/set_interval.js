442c7036515cfd8253a9173e6066abc8ddae2ba4
akshatverma1 <akshaverma14@gmail.com>
2024-06-03 17:52:21 +0530

    03/05/2024

---
+++ Part-7/set_interval.js
@@ -0,0 +1,11 @@
+// console.log("start");
+// setInterval(()=>{
+//     console.log("akshat");
+//     console.log("akshat");
+// },4000);
+// console.log("End");
+console.log("Hii There!");
+setTimeout(()=>{
+    console.log("Akshat")
+},4000);
+console.log("End");
