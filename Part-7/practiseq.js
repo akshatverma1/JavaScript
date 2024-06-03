5d8cac9f87233b39811fcf62abe6710d941554ae
akshatverma1 <akshaverma14@gmail.com>
2024-06-03 18:23:19 +0530

    03/05/2024

---
+++ Part-7/practiseq.js
@@ -0,0 +1,17 @@
+// let id1=setInterval(()=>{
+//     console.log("Akshat");
+// },2000);
+// setTimeout(()=>{
+//     clearInterval(id1);
+// },10000);
+let length = 4;
+function callback() {
+    console.log(this.length);
+}
+const object = {
+    length: 5,
+    method() {
+        callback();
+    },
+};
+object.method();
---
+++ Part-7/q1.js
@@ -0,0 +1,6 @@
+let ar=[2,2,2,2,2,2];
+array =(ar)=>{
+    let avg = (ar[0]+ar[1]+ar[2]+ar[3]+ar[4]+ar[5])/6;
+    return avg;
+}
+console.log(array(ar));
