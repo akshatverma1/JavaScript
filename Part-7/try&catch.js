d4af70cf85abbb6d2ba63db96def0dd3be5ee970
akshatverma1 <akshaverma14@gmail.com>
2024-06-03 16:11:25 +0530

    03/06/2024

---
+++ Part-7/try&catch.js
@@ -0,0 +1,11 @@
+console.log("Akshat");
+console.log("Akshat");
+console.log("Akshat");
+try {
+    console.log(a);
+}catch(d){
+    console.log(`(nothing & ${d})`);
+}
+console.log("Akshat");
+console.log("Akshat");
+console.log("Akshat");
