06eac3aad5f2a17b0dfa79c7e20ac24edd6e5c9a
akshatverma1 <akshaverma14@gmail.com>
2024-06-04 22:12:46 +0530

    messgae

--- Part-7/practiseq.js
+++ Part-7/practiseq.js
@@ -3,7 +3,7 @@ let id1=setInterval(()=>{
 },2000);
 setTimeout(()=>{
     clearInterval(id1);
-},1000000000);
+},10000000000);
 // let length = 4;
 // function callback() {
 //     console.log(this.length);
