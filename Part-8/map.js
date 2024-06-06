cfe4866d95c65cf78b39bc233d35bf3e64a0a6a8
akshatverma1 <akshaverma14@gmail.com>
2024-06-06 21:39:26 +0530

    06/06/2024

--- Part-8/map.js
+++ Part-8/map.js
@@ -1,5 +1,4 @@
 let number=[1,2,3,4,5,996];
 let double=number.map((element)=>{
-    console.log(element);
-    return element%2==0;
+    return element;
 });
---
+++ Part-8/q2.js
@@ -0,0 +1,7 @@
+// Qs2. Create a new array using the map function whose each element is equal to the
+// original element plus 5.
+let array =[1,2,3,4,5,6,7];
+let doublee = array.map((element)=>{
+    return element+5;
+})
+console.log(doublee);
