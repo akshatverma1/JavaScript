2348eaf9b42b351daf33b3bdfcb89e388dfe39a7
akshatverma1 <akshaverma14@gmail.com>
2024-06-06 13:21:12 +0530

    06/06/2024

---
+++ Part-8/practise.js
@@ -0,0 +1,4 @@
+let num = [1, 2, 3, 4, 5, 6, 7, 8, 90,80,50];
+let array_mul = num.filter((ele)=>{
+    return ele%10==0;
+});
--- Part-8/reduce.js
+++ Part-8/reduce.js
@@ -1,3 +1,11 @@
-let num=[1,2,3,4];
-let final_value = num.reduce((returnn,element)=> returnn+element);
-console.log(final_value);
+let num=[1,2,3,4,79,90,33,634,634,789];
+// let final_value = num.reduce((returnn,element)=> returnn+element);
+// console.log(final_value);
+let max = num.reduce(function maximum(max,element){
+    if(max<=element){
+        return element;
+    }else{
+        return max;
+    }
+});
+console.log(max);
