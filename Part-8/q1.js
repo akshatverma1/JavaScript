05a240bb6f9b6b93adc70c53758f95268af1b892
akshatverma1 <akshaverma14@gmail.com>
2024-06-06 21:30:50 +0530

    06/06/2024

--- Part-8/q1.js
+++ Part-8/q1.js
@@ -3,5 +3,11 @@
 let arr=[1,2,3,4,5,6];
 let newArray = arr.map((element)=>{
     return element*element;
-})                         
-console.log(newArray);                           
+})
+let avg,v;            
+console.log(newArray);
+let sum = arr.reduce((returnn,element)=>{
+    return returnn+element;
+});
+v = (sum/arr.length);
+console.log(v);
