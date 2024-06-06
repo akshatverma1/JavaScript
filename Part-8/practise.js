c2955ff07b5ce6333ff322ea403c21fb6a512070
akshatverma1 <akshaverma14@gmail.com>
2024-06-06 13:34:36 +0530

    06/06/2024

--- Part-8/practise.js
+++ Part-8/practise.js
@@ -1,4 +1,13 @@
-let num = [1, 2, 3, 4, 5, 6, 7, 8, 90,80,50];
-let array_mul = num.filter((ele)=>{
-    return ele%10==0;
+let num = [-91, 2, 3, 4, 5, 6, 7, 8, 90,80,50];
+// let array_mul = num.filter((ele)=>{
+//     return ele%10==0;
+// });
+let min_variable =0;
+let min = num.reduce((min_variable,element)=>{
+    if(min_variable<element){
+        return min_variable;
+    }else{
+        return element;
+    }
 });
+console.log(min);
