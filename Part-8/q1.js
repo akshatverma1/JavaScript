8b23bfe3fe3e0508f960ef9db8ab3a8ad5ac26d5
akshatverma1 <akshaverma14@gmail.com>
2024-06-06 21:05:03 +0530

    06/06/2024

---
+++ Part-8/q1.js
@@ -0,0 +1,7 @@
+// Qs1. Square and sum the array elements using the arrow function and then find the
+// average of the array
+let arr=[1,2,3,4,5,6];
+let newArray = arr.map((element)=>{
+    return element*element;
+})                         
+console.log(newArray);                           
