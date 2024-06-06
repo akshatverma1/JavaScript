5d4ef66d5791a9672a5b7cddf421fb6c6b8e956c
akshatverma1 <akshaverma14@gmail.com>
2024-06-06 15:34:18 +0530

    06/06/2024

--- Part-8/spread_method.js
+++ Part-8/spread_method.js
@@ -1,5 +1,20 @@
-let stringss = ["akshat"];
-function fff(strin){
+let stringss = "akshat";
+function fff(...strin){
     console.log(...strin);
+    return strin;
 }
-fff(stringss);
+let ff = fff(...stringss);
+console.log(ff[5]);
+console.log(..."verma");
+// let odd =[1,3,5,7,9];
+// let even =[2,4,6,8];
+// let rrr=[...odd,...even];
+// console.log(rrr);
+
+// spread eith object ilterals
+obj1={
+    name :"lakshya verma",
+    age : 13
+}
+let print = {...obj1,mobile:9109621850};
+console.log(print);
