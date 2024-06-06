10bab830156bede0debb54df7e3c581e000e59ec
akshatverma1 <akshaverma14@gmail.com>
2024-06-06 14:43:46 +0530

    06/06/2024

---
+++ Part-8/deafult_function.js
@@ -0,0 +1,12 @@
+//condition-1
+// function add(a,b=1){
+//     return a+b;
+// }
+// console.log(add(1,4));//5
+//condition-2
+function mul(a = 8, b) {
+    return a * b;
+}
+// if mul(1,4)//5
+// b=9
+// if mul(9)//then ouyput is a=9 & b=undefined
