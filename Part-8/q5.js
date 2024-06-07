b0ce6aaa46989ae32527c2b286bd107c64eb559a
akshatverma1 <akshaverma14@gmail.com>
2024-06-07 11:30:33 +0530

    07/06/2024

--- Part-8/q5.js
+++ Part-8/q5.js
@@ -1,6 +1,8 @@
 // Qs5. Write a function called mergeObjects that accepts two objects and returns a new
 // object which contains all the keys and values of the first object and second object.
 function mergeObjects(obj1,obj2){
-    console.log({...obj1, ...obj2});
+    // console.log({...obj1, ...obj2});
+    let ee = {...obj1, ...obj2};
+    console.log(ee);
 }
 mergeObjects({a:1,b:2},{c:3,d:4});
