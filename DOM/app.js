45814bda73b46771a858e27600e18500cdef12ba
akshatverma1 <akshaverma14@gmail.com>
2024-06-08 03:27:51 +0530

    08/06/2024

--- DOM/app.js
+++
@@ -1,5 +0,0 @@
-let oldddd = document.getElementsByClassName("oldImg");
-for (let i = 0; i < oldddd.length; i++) {
-    console.log((oldddd[i]));
-}
-console.log(document.querySelectorAll(".oldImg"));
