8dc3114167cc8db72ff97fccc4130e07b6868d49
akshatverma1 <akshaverma14@gmail.com>
2024-06-08 00:05:03 +0530

    08/06/2024

--- DOM/app.js
+++ DOM/app.js
@@ -1,8 +1,5 @@
 let oldddd = document.getElementsByClassName("oldImg");
-for (let i=0;i<oldddd.length;i++){
+for (let i = 0; i < oldddd.length; i++) {
     console.log((oldddd[i]));
 }
-document.querySelector('p');
-document.querySelector('#myId');
-document.querySelector('.myclass');
-document.querySelectorAll("p");
+console.log(document.querySelectorAll(".oldImg"));
