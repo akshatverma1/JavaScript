83762841ca08b4078052ae23b70f3f187f6f3132
akshatverma1 <akshaverma14@gmail.com>
2024-06-08 13:19:29 +0530

    08/06/2024

---
+++ DOM/q2.js
@@ -0,0 +1,5 @@
+let username = document.createElement("input");
+let body = document.querySelector("body");
+body.appendChild(username);
+username.innerText="Input Box";
+username.placeholder="User Name";
