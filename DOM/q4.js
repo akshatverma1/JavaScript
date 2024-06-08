ef69cd5fa9cac10efe030044bf7a37d60b043fe0
akshatverma1 <akshaverma14@gmail.com>
2024-06-08 13:45:04 +0530

    08/06/2024

---
+++ DOM/q4.js
@@ -0,0 +1,6 @@
+let body = document.querySelector("body");
+let h1 = document.createElement("h1");
+body.appendChild(h1);
+h1.innerText="DOM Practise";
+h1.style.textDecoration="underline";
+h1.style.textDecorationColor="purple";
