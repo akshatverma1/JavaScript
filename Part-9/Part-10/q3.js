c0d10a049d04c2cc92b14c53a83bf2e6bc9d42ac
akshatverma1 <akshaverma14@gmail.com>
2024-06-09 12:39:23 +0530

    09/06/2024

---
+++ Part-9/Part-10/q3.js
@@ -0,0 +1,7 @@
+let input = document.querySelector("input");
+let h1 = document.querySelector("h1");
+input.addEventListener("input",function(){
+    h1.innerText=input.value;
+    console.log(input.value);
+
+});
