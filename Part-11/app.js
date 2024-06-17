561f2036ab62ed0a80cb8d3e2265b2e6c7a8d8bf
akshatverma1 <akshaverma14@gmail.com>
2024-06-17 13:51:01 +0530

    17/06/2024

--- Part-11/app.js
+++ Part-11/app.js
@@ -1,4 +1,9 @@
-setTimeout(function(){
-    console.log("Akshat");
-},2000);
-console.log("hello!");
+let h1 = document.querySelector("h1");
+function changeColor(delay,color){
+    setTimeout(function (){
+        h1.style.color = color;
+    },delay);
+}
+changeColor(1000,"red");
+changeColor(2000,"blue");
+changeColor(3000,"green");
---
+++ Part-11/callback.js
@@ -0,0 +1,12 @@
+let h1 = document.querySelector("h1");
+function changeColor(color,delay,nextChangeColor){
+    setTimeout(function (){
+        h1.style.color=color;
+        nextChangeColor();
+    },delay);
+}
+changeColor("red",1000,()=>{
+    changeColor("violet",1000,()=>{
+        changeColor("green",1000);
+    });
+})
