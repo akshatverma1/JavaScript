516a93631ed5f84163d3152867cc0a09b7b1ef8d
akshatverma1 <akshaverma14@gmail.com>
2024-06-22 22:30:21 +0530

    21/06/2024

--- Part-11/promises_improved.js
+++ Part-11/promises_improved.js
@@ -38,6 +38,7 @@ savetodb("Akshat").then(()=>{
     console.log("akshat");
     console.log("akshat");
     console.log("akshat");
+    console.log("akshat");
 }).catch(()=>{
     console.log("Data is not stored");
 })
