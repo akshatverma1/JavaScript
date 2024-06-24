246e0947058d08c59e7289a2de7c73d9100f05c6
akshatverma1 <akshaverma14@gmail.com>
2024-06-25 01:04:55 +0530

    25/06/2024

--- Part-13/axioos.js
+++ Part-13/axioos.js
@@ -1,10 +1,15 @@
+let button = document.querySelector("button");
+let paragraph = document.querySelector("p");
+button.addEventListener("click",()=>{
+    apies();
+})
 let url = "https://catfact.ninja/fact";
 async function apies() {
     try {
         let appp = await axios.get(url);
         console.log(appp.data.fact);
+        paragraph.innerText=appp.data.fact;
     } catch (err) {
         console.log("Error = " + err);
     }
 }
-apies();
