d262599ed2fd33fb73ba08605db74b86d6370388
akshatverma1 <akshaverma14@gmail.com>
2024-08-21 20:57:06 +0530

    20/08/2024

--- array.js
+++ array.js
@@ -2,3 +2,4 @@ let student=["akshat","verma","akshu"];
 let example=["a","b","c","d","e","g"];
 let months=["january","july","march","augest"];
 let technology=["c","c++","html","javascript","python","java","c#","sql"];
+let akshat =["aa","dfs","dfs"];
