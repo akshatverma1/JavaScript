5761955b79c6c490543b691c954e280f424841bc
akshatverma1 <akshaverma14@gmail.com>
2024-08-21 20:58:56 +0530

    20/08/2024

--- array.js
+++ array.js
@@ -3,3 +3,4 @@ let example=["a","b","c","d","e","g"];
 let months=["january","july","march","augest"];
 let technology=["c","c++","html","javascript","python","java","c#","sql"];
 let akshat =["aa","dfs","dfs"];
+let krishnaji=["Akshat"];
