af3cd0dfb53ccc5835aa0569cd87add73d48869a
akshatverma1 <akshaverma14@gmail.com>
2024-08-22 20:06:17 +0530

    20/08/2024

--- array.js
+++ array.js
@@ -4,3 +4,4 @@ let months=["january","july","march","augest"];
 let technology=["c","c++","html","javascript","python","java","c#","sql"];
 let akshat =["aa","dfs","dfs"];
 let krishnaji=["Akshat","Lakshya"];
+let akshatT =[];
