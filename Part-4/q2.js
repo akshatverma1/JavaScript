9c4ddcf30b58f82dd2e9681d8a91497c808c27dc
akshatverma1 <akshaverma14@gmail.com>
2024-08-24 18:48:46 +0530

    24/08/2024

--- Part-4/q2.js
+++ Part-4/q2.js
@@ -1,4 +1,6 @@
 // Write a JS program to find the no of digits in a number
+
+
 let number = 287152;
 let count=0;
 let copy=number;
@@ -7,3 +9,5 @@ while(copy>0){
     copy= Math.floor(copy/10);
 }
 console.log(count);
+let msg ="     akshat    ";
+msg.trim();
