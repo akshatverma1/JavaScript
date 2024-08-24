626e17faac6051813f5ca430deffd360b1b2d835
akshatverma1 <akshaverma14@gmail.com>
2024-08-25 04:11:40 +0530

    25/08/2024

--- Part-4/q2.js
+++ Part-4/q2.js
@@ -14,6 +14,26 @@ let str = "I love coding";
 // str.indexOf("love");
 let car =["Audi","Mercedes","BMW","Xylo","Santro","Tata safari","ven"];
 
-function func(){
+let rr = function func(){
     console.log("Akshat");
+}
+let arrayy = [1,2,3,4,5,6];
+let double = arrayy.filter((knoww)=>{
+    console.log(knoww);
+    return 2;
+})
+let strruu = "keyboard";
+function spreadd(...stryrty){
+    console.log("spread");
+    return stryrty;
+}
+let a = spreadd(...strruu);
+function ddd(data){
+    return new Promise((Success,failure)=>{
+        if(data>3){
+            Success("Data is Entered");
+        }else {
+            failure("Data does not Stored");
+        }
+    })
 }
