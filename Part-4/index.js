04cb4fb75baab4c6847f4e945a93894894d51e91
akshatverma1 <akshaverma14@gmail.com>
2024-08-25 13:42:01 +0530

    25/08/2024

---
+++ Part-4/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+let port = 3;
+
+app.listen(port,()=>{
+    console.log("this is web server");
+})
+app.use((req,res)=>{
+    console.log("request received");
+    res.send("lakshay is a pagal man");
+})
