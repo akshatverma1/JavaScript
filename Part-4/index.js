2cef6860d0bb0865cc0976a4b3c9c411b2f79572
akshatverma1 <akshaverma14@gmail.com>
2024-08-25 15:51:31 +0530

    25/08/2024

--- Part-4/index.js
+++ Part-4/index.js
@@ -8,7 +8,20 @@ app.listen(port ,()=>{
     console.log("this is first web server");
 });
 
-app.use((req,res)=>{
-    console.log("request received");
-    res.send("akshat");
-});
+// app.use((req,res)=>{
+//     console.log("request received");
+//     res.send("akshat");
+// });
+
+app.get("/apple",(req,res)=>{
+    console.log(req.params);
+    res.send("akshata pallljfhgvvkufvbihr");
+})
+
+app.get("*",(req,res)=>{
+    res.send("akshata pallljfhgvvkufvbihr");
+})
+app.get("/ig/:username",(req,res)=>{
+    let {username} = req.params;
+    res.send(`${username}`);
+})
