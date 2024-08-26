1930d6ee8540f51ac4238366338a06faeeb2724a
akshatverma1 <akshaverma14@gmail.com>
2024-08-26 22:26:59 +0530

    26/08/2024

--- CURDAPI/index.js
+++ CURDAPI/index.js
@@ -7,11 +7,15 @@ const port = 3;
 //id generator
 const { v4: uuidv4 } = require('uuid');
 //
-
+let bodyParser = require('body-parser');
+app.use(bodyParser.json());
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 //For using EJS
 const path = require("path");
+
 app.set("View engine","ejs");
-app.set("views",path.join(__dirname,"/views"));
+app.set("views",path.join(__dirname,"views"));
+app.set(express.static(path.join(__dirname,"public")));
 //
 //For Post Request
 app.use(express.urlencoded({extended:true}));
@@ -35,22 +39,22 @@ post = [
     }
 ];
 
-
+//server start
 app.listen(port,()=>{
     console.log("Server is Working Condition");
 });
+//
 
-
+//see all posts
 app.get("/",(req,res)=>{
     res.render("main.ejs",{post});
 });
+//
 
-
+//making new post
 app.get("/new",(req,res)=>{
     res.render("new.ejs");
 });
-
-
 app.post("/new",(req,res)=>{
     let {username,content} = req.body;
     let id = uuidv4();
@@ -58,3 +62,34 @@ app.post("/new",(req,res)=>{
     console.log(username);
     res.redirect("http://localhost:3/");
 });
+//
+
+
+
+//for paricular open post
+app.get("/open/:id",(req,res)=>{
+    let {id} = req.params;
+    console.log(id);
+    let posts = post.find((p) => id===p.id); 
+    res.render("open.ejs",{posts});
+});
+//
+
+//For Update
+app.patch("/update/:id",(req,res)=>{
+    let {id} = req.params;
+    let newcontent = req.body;
+    let posts = post.find((p) => id===p.id);
+    posts.content = newcontent;
+    console.log(posts.content);
+    res.send("patch request accept");
+})
+
+
+
+app.get("/update/:id",(req,res)=>{
+    let {id} = req.params;
+    let newcontent = req.body.content;
+    let posts = post.find((p) => id===p.id); 
+    res.render("update.ejs",{posts});
+})
