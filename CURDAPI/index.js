36f84df3bd349c0b9a6a389211d6b9c2cfb6fe5d
akshatverma1 <akshaverma14@gmail.com>
2024-08-26 15:10:29 +0530

    26/08/2024

--- CURDAPI/index.js
+++ CURDAPI/index.js
@@ -1,13 +1,48 @@
+//For using express
 const express = require("express");
 const app = express();
+const port = 3;
+//
 
-const port =3;
+//id generator
+const { v4: uuidv4 } = require('uuid');
+//
+
+//For using EJS
+const path = require("path");
+app.set("View engine","ejs");
+app.set("views",path.join(__dirname,"/views"));
+//
+
+
+post = [
+    {
+        id :uuidv4(),
+        username : "akshat",
+        content : "Akshat is a Green Flag Boy."
+    }
+    ,{
+        id :uuidv4(),
+        username : "computer",
+        content : "Computer is a very smart Machine."
+    }
+    ,{
+        id :uuidv4(),
+        username : "onkaam",
+        content : "onkaam is a billion dollor company own by Akshat Verma."
+    }
+];
+
 
 app.listen(port,()=>{
     console.log("Server is Working Condition");
-})
+});
 
 app.get("/",(req,res)=>{
-    res.send("Akshat");
-    console.log("console");
-})
+    res.render("main.ejs",{post});
+});
+
+app.get("/new",(req,res)=>{
+    res.render("new.ejs");
+    // res.send("New Post Page"); 
+});
